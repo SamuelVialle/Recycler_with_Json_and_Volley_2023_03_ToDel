167db3bd493e56c4272c3886c1c52473cb1bb07b
Samuel Vialle <samuelvialle@gmail.com>
2023-11-13 15:41:04 +0100

    Initial commit

---
+++ app/src/main/java/com/samuelvialle/a_2023_03_recyclerwithjsonandvolley/MainActivity.java
@@ -0,0 +1,14 @@
+package com.samuelvialle.a_2023_03_recyclerwithjsonandvolley;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
